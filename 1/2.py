e1f3c29e3c8de39b9c27cfcba592037ccc7d9fd7
Tyler Lindberg <tylindberg@hotmail.com>
2023-12-01 17:12:10 -0800

    Add Day 1 documentation

--- 1/2.py
+++ 1/2.py
@@ -16,6 +16,19 @@ DIGIT_STRINGS: Final[List[str]] = [
 
 
 def check_for_digit_string(char_buffer: str) -> str:
+    """
+    Check for a string representation of a digit (e.g. "zero", "one", ..., or "nine") in the provided string
+
+    Parameters
+    ----------
+    char_buffer : str
+        The string to search for a digit within
+
+    Returns
+    -------
+    str
+        Digit as string (e.g. "0", "1", ..., "9") or "" if no digit was found
+    """
     if len(char_buffer) < 3:
         return ""
 
@@ -27,6 +40,21 @@ def check_for_digit_string(char_buffer: str) -> str:
 
 
 def find_digit(line: str, reverse: bool) -> str:
+    """
+    Find the first digit from a string, either character string or number
+
+    Parameters
+    ----------
+    line : str
+        A line from the calibration file (okay to have newline at the end)
+    reverse : bool
+        Whether or not to parse the string in reverse
+
+    Returns
+    -------
+    str
+        The digit as a string (e.g. "0", "1", ..., or "9")
+    """
     found_digit = ""
     char_buffer = ""
 
@@ -37,6 +65,7 @@ def find_digit(line: str, reverse: bool) -> str:
             break
 
         char_buffer = f"{char_buffer}{char}" if not reverse else f"{char}{char_buffer}"
+        # We check for this first to avoid overwriting the value found by the `isdigit` section
         found_digit = check_for_digit_string(char_buffer)
 
         if char.isdigit():
@@ -46,6 +75,19 @@ def find_digit(line: str, reverse: bool) -> str:
 
 
 def compute_calibration_value(line: str) -> int:
+    """
+    Return integer calibration value from a given line
+
+    Parameters
+    ----------
+    line: str
+        A line from the calibration file (okay to have newline at the end)
+
+    Returns
+    -------
+    int
+        The computed calibration value
+    """
     left_digit = find_digit(line, reverse=False)
     right_digit = find_digit(line, reverse=True)
 
