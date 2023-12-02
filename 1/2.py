22a8d762d65f91dc0014c981985a17ee540c4b9d
Tyler Lindberg <tylindberg@hotmail.com>
2023-12-01 16:51:47 -0800

    Improvements on Day 1

--- 1/2.py
+++ 1/2.py
@@ -15,49 +15,39 @@ DIGIT_STRINGS: Final[List[str]] = [
 ]
 
 
-def check_for_digit_string(char_buffer: str) -> str | None:
+def check_for_digit_string(char_buffer: str) -> str:
     if len(char_buffer) < 3:
-        return None
+        return ""
 
     for i in range(10):
         if char_buffer.find(DIGIT_STRINGS[i]) != -1:
             return str(i)
 
-    return None
+    return ""
+
+
+def find_digit(line: str, reverse: bool) -> str:
+    found_digit = ""
+    char_buffer = ""
+
+    line_iter = line if not reverse else reversed(line)
+
+    for char in line_iter:
+        if found_digit != "":
+            break
+
+        char_buffer = f"{char_buffer}{char}" if not reverse else f"{char}{char_buffer}"
+        found_digit = check_for_digit_string(char_buffer)
+
+        if char.isdigit():
+            found_digit = char
+
+    return found_digit
 
 
 def compute_calibration_value(line: str) -> int:
-    left_digit = ""
-    right_digit = ""
-
-    char_buffer = ""
-
-    for char in line:
-        if left_digit != "":
-            break
-
-        if char.isdigit():
-            left_digit = char
-
-        char_buffer = f"{char_buffer}{char}"
-        found_char = check_for_digit_string(char_buffer)
-        if found_char is not None:
-            left_digit = found_char
-
-    char_buffer = ""
-
-    for char in reversed(line):
-        if right_digit != "":
-            break
-
-        if char.isdigit():
-            right_digit = char
-
-        # We reverse the order we add to the buffer here to
-        char_buffer = f"{char}{char_buffer}"
-        found_char = check_for_digit_string(char_buffer)
-        if found_char is not None:
-            right_digit = found_char
+    left_digit = find_digit(line, reverse=False)
+    right_digit = find_digit(line, reverse=True)
 
     return int(f"{left_digit}{right_digit}")
 
