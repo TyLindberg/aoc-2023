92c512d3f8029d3315b82959290120411528abc6
Tyler Lindberg <tylindberg@hotmail.com>
2023-12-04 00:05:17 -0800

    Day 1: More pythonic

--- 1/2.py
+++ 1/2.py
@@ -1,23 +1,25 @@
-from typing import Final, List
+import re
+from typing import Final
 
 
-DIGIT_STRINGS: Final[List[str]] = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
+DIGIT_REGEXES: Final = [
+    re.compile("(0|zero)"),
+    re.compile("(1|one)"),
+    re.compile("(2|two)"),
+    re.compile("(3|three)"),
+    re.compile("(4|four)"),
+    re.compile("(5|five)"),
+    re.compile("(6|six)"),
+    re.compile("(7|seven)"),
+    re.compile("(8|eight)"),
+    re.compile("(9|nine)"),
 ]
 
 
 def check_for_digit_string(char_buffer: str) -> str:
     """
-    Check for a string representation of a digit (e.g. "zero", "one", ..., or "nine") in the provided string
+    Check for a string representation of a digit (e.g. "zero", "0", "one", "1", ..., "nine", or "9") in the provided
+    string
 
     Parameters
     ----------
@@ -29,11 +31,8 @@ def check_for_digit_string(char_buffer: str) -> str:
     str
         Digit as string (e.g. "0", "1", ..., "9") or "" if no digit was found
     """
-    if len(char_buffer) < 3:
-        return ""
-
     for i in range(10):
-        if char_buffer.find(DIGIT_STRINGS[i]) != -1:
+        if DIGIT_REGEXES[i].search(char_buffer) is not None:
             return str(i)
 
     return ""
@@ -56,20 +55,24 @@ def find_digit(line: str, reverse: bool) -> str:
         The digit as a string (e.g. "0", "1", ..., or "9")
     """
     found_digit = ""
-    char_buffer = ""
+    char_buffer_array = [""] * 5
+    insert_index = 5
+    remove_index = 0
+    if reverse:
+        insert_index, remove_index = remove_index, insert_index
 
     line_iter = line if not reverse else reversed(line)
 
     for char in line_iter:
-        if found_digit != "":
-            break
+        char_buffer_array.insert(insert_index, char)
+        char_buffer_array.pop(remove_index)
 
-        char_buffer = f"{char_buffer}{char}" if not reverse else f"{char}{char_buffer}"
-        # We check for this first to avoid overwriting the value found by the `isdigit` section
+        char_buffer = "".join(char_buffer_array)
+
         found_digit = check_for_digit_string(char_buffer)
 
-        if char.isdigit():
-            found_digit = char
+        if found_digit != "":
+            break
 
     return found_digit
 
@@ -95,12 +98,8 @@ def compute_calibration_value(line: str) -> int:
 
 
 def main():
-    acc = 0
     with open("1.txt", "r") as f:
-        for line in f:
-            acc += compute_calibration_value(line)
-
-    print(acc)
+        print(sum(compute_calibration_value(line.rstrip()) for line in f))
 
 
 if __name__ == "__main__":
